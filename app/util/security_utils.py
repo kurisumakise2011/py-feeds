98d2ad8aabff1af7d2241ae7a33c658f6b1edeb1
Vladyslav Nahaiev <vlad.nagae.vn@gmail.com>
2019-11-24 18:16:49 +0200

    Fix invalid salt

--- app/util/security_utils.py
+++ app/util/security_utils.py
@@ -10,6 +10,8 @@ def generate_password_hash(password):
 def check_password_hash(plain_password, password_hash):
     plain_password_bin = plain_password.encode('utf-8')
     password_hash_bin = password_hash.encode('utf-8')
-    is_correct = bcrypt.checkpw(plain_password_bin, password_hash_bin)
-    return is_correct
-
+    try:
+        is_correct = bcrypt.checkpw(plain_password_bin, password_hash_bin)
+        return is_correct
+    except ValueError:
+        return False
