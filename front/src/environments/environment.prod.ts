75b57309d6225ac2a088e948d94ead5d3d635636
Vladyslav Nahaiev <vlad.nagae.vn@gmail.com>
2019-11-25 10:53:14 +0200

    Update url

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://web',
+  apiUrl: 'http://192.168.99.100:8080',
   githubsso: 'https://github.com/login/oauth/authorize?client_id=5ea9a5cd8777ed7145bf',
-};
+}; 
